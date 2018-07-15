ff65a4f7aeaf7e6f10bd6a2c2c0df77473ccbc3a
Chorokichi <kdy.developer@gmail.com>
2018-07-16 00:33:14 +0900

    [add] app2: just display localhost:8080 page

--- src/main/main.js
+++ src/main/main.js
@@ -7,7 +7,7 @@ let mainWindow
 console.log("process.platform: " + process.platform)
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({ width: 800, height: 600 })
+    mainWindow = new BrowserWindow({ width: 1200, height: 900 })
 
     // and load the index.html of the app.
     let appName
@@ -26,12 +26,12 @@ function createWindow() {
         process.exit(1);
     } else if (appName === "renderer-hello-world") {
         mainWindow.loadFile('src/renderer-hello-world/index.html')
+    } else if (appName === "renderer-jenkins") {
+        mainWindow.loadFile('src/renderer-jenkins/index.html')
     } else {
         console.error('This app is not supporetd!!!');
         process.exit(1);
     }
-
-
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
@@ -42,6 +42,11 @@ function createWindow() {
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         mainWindow = null
+    })
+
+    mainWindow.on('resize', function () {
+        const message = `Size: ${mainWindow.getSize()} Position: ${mainWindow.getPosition()}`
+        console.log(message)
     })
 }
 
